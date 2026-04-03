ec908d4a27a0b8614cce2d86edd788183c500113
Sunil T <sthakur2@collin.edu>
2026-04-02 20:32:52 -0500

    Update UserList.jsx

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -43,7 +43,7 @@ function UserList() {
     }
 
     // 🔹 Simulating backend API using professor API
-    fetch("https://db-conn-email.onrender.com/users_sam", {
+    fetch("https://db-conn-email.onrender.com/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
